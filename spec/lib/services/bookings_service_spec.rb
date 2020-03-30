338bb5eb1b5be2fa1d21fdfd98f40fce028be45a
Daniel Uribe <du@medipply.com>
2020-03-30 07:49:24 -0500

    add improvements on models and migrations

---
+++ spec/lib/services/bookings_service_spec.rb
@@ -0,0 +1,9 @@
+require 'services/bookings_service'
+
+RSpec.describe BookingsService do
+    context 'bookings services work as expected' do
+        it 'gets the expected bookings' do
+            expect(5).to eq 5
+        end
+    end
+end
